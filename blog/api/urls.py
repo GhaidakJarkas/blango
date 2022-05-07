65603a7ff79d7d5ec905091d88b9c5ab843517f4
Ghaidak Jarkas <ghaidokajarkas@gmail.com>
2022-05-07 20:39:51 +0000

    Finish Viewsets and routers

--- blog/api/urls.py
+++ blog/api/urls.py
@@ -1,12 +1,17 @@
 from django.urls import path, include, re_path
 from rest_framework.urlpatterns import format_suffix_patterns
-from blog.api.views import PostList, PostDetail, UserDetail
+from blog.api.views import  UserDetail, TagViewSet, PostViewSet #PostList, PostDetail,
 from rest_framework.authtoken import views
 import os 
 from drf_yasg import openapi
 from drf_yasg.views import get_schema_view
+from rest_framework.routers import DefaultRouter
 
 
+
+router = DefaultRouter()
+router.register("tags", TagViewSet)
+router.register("posts", PostViewSet)
 
 schema_view = get_schema_view(
   openapi.Info(
@@ -20,8 +25,8 @@ schema_view = get_schema_view(
 
 
 urlpatterns = [
-  path("posts/", PostList.as_view(), name="api_post_list"),
-  path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
+  # path("posts/", PostList.as_view(), name="api_post_list"),
+  # path("posts/<int:pk>", PostDetail.as_view(), name="api_post_detail"),
   path("users/<str:email>", UserDetail.as_view(), name = "api_user_detail"),
 ]
 
@@ -34,10 +39,11 @@ urlpatterns += [
         r"^swagger(?P<format>\.json|\.yaml)$",
         schema_view.without_ui(cache_timeout=0),
         name="schema-json",
-    ),
-    path(
+  ),
+  path(
         "swagger/",
         schema_view.with_ui("swagger", cache_timeout=0),
         name="schema-swagger-ui",
-    ),
+  ),
+  path("", include(router.urls)),
 ]
